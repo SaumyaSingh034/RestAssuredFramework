23b9809239fc288bea240d34b29769026f69c65a
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-30 15:26:00 +0530

    Base Test Completed

---
+++ src/main/java/propConfig/Configuration.java
@@ -0,0 +1,42 @@
+package propConfig;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+
+    private Properties prop;
+    private FileInputStream ip;
+
+    public Properties initProp() {
+
+//		if (prop == null) {
+        prop = new Properties();
+        try {
+            ip = new FileInputStream("./src/test/resources/config/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //}
+
+        return prop;
+    }
+
+    public String getProperty(String key) {
+        initProp();
+        return prop.getProperty(key);
+    }
+
+    public void setProperty(String key, String value) {
+        System.out.println("setttt");
+        initProp();
+        prop.setProperty(key, value);
+        System.out.println(prop);
+    }
+
+}
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,34 @@
+package base;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import propConfig.Configuration;
+import restClient.RestClient;
+
+import java.util.Properties;
+
+public class BaseTest {
+
+
+    protected static final String GOREST_ENDPOINT = "/public/v2/users/";
+    protected static final String CIRCUIT_ENDPOINT = "api/f1/2017/circuits.json";
+    protected static final String REQRES_ENDPOINT = "/api/users/2";
+    protected static final String PRODUCT_ENDPOINT = "/products";
+    protected static final String FLIGHT_DESTINATIONS = "/v1/shopping/flight-destinations";
+    protected static final String AMADEUS_AUTH_ENDPOINT = "/v1/security/oauth2/token";
+
+    protected RestClient restClient;
+    protected Properties prop;
+    protected Configuration config;
+
+    @Parameters({"baseURI"})
+    @BeforeTest
+    public void setUp(String baseURI) {
+        RestAssured.filters(new AllureRestAssured());
+        config = new Configuration();
+        prop = config.initProp();
+        restClient = new RestClient(prop, baseURI);
+    }
+}
